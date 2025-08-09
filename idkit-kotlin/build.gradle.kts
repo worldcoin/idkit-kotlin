d9b2d417f2b8b1c1c6e8e88b5ee9bb8e8383a1cd
Seán Olszewski <sean.olszewski@toolsforhumanity.com>
2025-08-08 17:19:49 -0700

    [ID-643] Update idkit-kotlin to Kotlin 2.0
    * Remove the demo app as it doesn't exemplify the SDK
    * Simplify the build setup for `idkit-kotlin` so it doesn't include Compose
    * Remove unneeded dependencies to speed up build time and Gradle file syncing
    * Update .gitignore
    * Remove an accidentally committed `local.properties`

--- idkit-kotlin/build.gradle.kts
+++ idkit-kotlin/build.gradle.kts
@@ -11,18 +11,11 @@ android {
 
     defaultConfig {
         minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
@@ -35,13 +28,7 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlincrypto.sha3)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,5 +20,4 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "IDKitDev"
-include(":app")
 include(":idkit-kotlin")
