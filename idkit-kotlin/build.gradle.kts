c6a3838a90c14ef716c95ddcce850d9c0d956572
manutfh <manu.singh@toolsforhumanity.com>
2025-09-11 12:14:39 -0700

    Add publishing to gradle, gitnore local.prop

--- idkit-kotlin/build.gradle.kts
+++ idkit-kotlin/build.gradle.kts
@@ -25,10 +25,41 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
     implementation(libs.material)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlincrypto.sha3)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.worldcoin"
+                artifactId = "idkit-kotlin"
+                version = "1.0.2"
+
+                from(components["release"])
+            }
+        }
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/worldcoin/idkit-kotlin")
+                credentials {
+                    username = System.getenv("GITHUB_USER")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+    }
 }
